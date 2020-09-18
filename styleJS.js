8c38f8af8ed3ea7dd2fb6d1770b2f59b415c9bdf
Kristin Brumfield <kristinbrumfield@Kbrummy.local>
2020-09-18 15:54:42 -0700

    commit

--- styleJS.js
+++ styleJS.js
@@ -7,7 +7,7 @@ $("#searchButton").on("click", function () {
 function searchWeather(city) {
   console.log(city);
   var search =
-    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
     city +
     "&appid=b7ef947311647ad100ad8f8b2cd6da49";
   $.get(search, function (data) {
