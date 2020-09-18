a921e6dc572818b61fd0d952e994902ca39b537b
Kristin Brumfield <kristinbrumfield@Kbrummy.local>
2020-09-18 15:45:47 -0700

    commit

--- styleJS.js
+++ styleJS.js
@@ -1,9 +1,17 @@
-$(document).ready(function () {
-  $("#searchButton").on("click", function () {
-    var searchVal = $("#searchInput").val();
+$("#searchButton").on("click", function () {
+  var searchVal = $("#searchInput").val();
 
-    $("#searchInput").val("");
-    //console.log(searchVal);
-    searchWeather(searchVal);
-  });
+  searchWeather(searchVal);
 });
+
+function searchWeather(city) {
+  console.log(city);
+  var search =
+    "api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=b7ef947311647ad100ad8f8b2cd6da49";
+  $.get(search, function (data) {
+    console.log(data);
+    alert("Load was performed.");
+  });
+}
